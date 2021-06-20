cb2be7934685377898f8bf39f52a342dfbf7f55d
Mankamal Mishra <mishramankamal1@gmail.com>
2021-06-20 19:59:10 +0530

    renamed file

--- notification.py
+++ notification.py
@@ -1,8 +1,9 @@
 import telegram
 import constant
+import os
 
 
 def send_to_telegram(news):
-    bot = telegram.Bot(token=constant.BOT_API_KEY)
+    bot = telegram.Bot(token=os.environ.get("API_KEY"))
     status = bot.send_message(chat_id=constant.MY_CHANNEL_NAME, text=news,
                               parse_mode=telegram.ParseMode.HTML, disable_web_page_preview=True)
