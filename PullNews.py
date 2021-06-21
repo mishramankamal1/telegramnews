09712fc62944c1475f071764af5266163e746254
Mankamal Mishra <mishramankamal1@gmail.com>
2021-06-21 19:42:15 +0530

    Date/Time Changed

--- PullNews.py
+++ PullNews.py
@@ -5,7 +5,7 @@ from pygooglenews import GoogleNews
 from datetime import datetime
 import constant
 import time
-
+import pytz
 
 def short_url(url):
     shortner = pyshorteners.Shortener()
@@ -22,18 +22,14 @@ while True:
         news_story.append([entries['title'], entries['link']])
 
     panda_dataframe = pd.DataFrame(news_story[:10], columns=['news', 'link'])
-
     panda_dataframe['short_url'] = panda_dataframe['link'].apply(short_url)
     panda_new = panda_dataframe.drop(['link'], axis=1)
     panda_new["news_url"] = panda_new["news"] + "\n<b>Link: " + panda_new['short_url'] + "</b>"
-
     news_story_str = '\n\n'.join(str(e) for e in panda_new["news_url"])
-
-    date_now = datetime.now().strftime("%I:%M %p")
+    tz_NY = pytz.timezone('Asia/Kolkata')
+    date_now = datetime.now(tz_NY).strftime("%I:%M %p")
     news_time = "<b>News @ {}</b>".format(date_now)
-
     news_str = news_time + constant.news_message + "\n\n\n" + news_story_str
-
     notification.send_to_telegram(news_str)
 
     time.sleep(60 * 60)
